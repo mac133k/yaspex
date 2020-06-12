8d439ea470a32ea050cf0c38daee3c400a438ea0
spiff <mspinoff@shearwatergeo.com>
2020-06-12 17:53:21 +0000

    CPU and node metric names pluralized for uniformity.

--- jobs.py
+++ jobs.py
@@ -13,12 +13,12 @@ class JobInfoCollector(object):
 	def collect(self):
 		# Metric declarations
 		JOBS_NUM = GaugeMetricFamily('jobs_num', 'Numbers of jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
-		JOBS_CPU_REQ = GaugeMetricFamily('jobs_cpu_req', 'Numbers of CPUs requested for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
-		JOBS_CPU_ALLOC = GaugeMetricFamily('jobs_cpu_alloc', 'Numbers of CPUs allocated for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
+		JOBS_CPUS_REQ = GaugeMetricFamily('jobs_cpus_req', 'Numbers of CPUs requested for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
+		JOBS_CPUS_ALLOC = GaugeMetricFamily('jobs_cpus_alloc', 'Numbers of CPUs allocated for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
 		JOBS_MEM_REQ = GaugeMetricFamily('jobs_mem_req', 'Amounts of memory requested for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels, unit='bytes')
 		JOBS_MEM_ALLOC = GaugeMetricFamily('jobs_mem_alloc', 'Amounts of memory allocated for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels, unit='bytes')
-		JOBS_NODE_REQ = GaugeMetricFamily('jobs_node_req', 'Numbers of nodes requested for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
-		JOBS_NODE_ALLOC = GaugeMetricFamily('jobs_node_alloc', 'Numbers of nodes allocated for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
+		JOBS_NODES_REQ = GaugeMetricFamily('jobs_nodes_req', 'Numbers of nodes requested for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
+		JOBS_NODES_ALLOC = GaugeMetricFamily('jobs_nodes_alloc', 'Numbers of nodes allocated for jobs in the cluster grouped by {}'.format(', '.join(self.labels)), labels=self.labels)
 		
 		# Load job info from Slurm
 		jobs = pyslurm.job().get()
@@ -26,13 +26,13 @@ class JobInfoCollector(object):
 		# Translate user IDs to names
 		jobdf['user'] = jobdf.user_id.apply(lambda uid: pwd.getpwuid(uid).pw_name)
 		# Extract TRES req
-		jobdf['cpu_req'] = jobdf.tres_req_str.str.extract(r'cpu=(?P<cpu_req>[0-9]+)').fillna(0).astype(int) 
+		jobdf['cpus_req'] = jobdf.tres_req_str.str.extract(r'cpu=(?P<cpus_req>[0-9]+)').fillna(0).astype(int) 
 		jobdf['mem_req'] = jobdf.tres_req_str.str.extract(r'mem=(?P<mem_req>[0-9]+)').fillna(0).astype(int) 
-		jobdf['node_req'] = jobdf.tres_req_str.str.extract(r'node=(?P<node_req>[0-9]+)').fillna(0).astype(int) 
+		jobdf['nodes_req'] = jobdf.tres_req_str.str.extract(r'node=(?P<nodes_req>[0-9]+)').fillna(0).astype(int) 
 		# Extract TRES alloc
-		jobdf['cpu_alloc'] = jobdf.tres_alloc_str.str.extract(r'cpu=(?P<cpu_alloc>[0-9]+)').fillna(0).astype(int) 
+		jobdf['cpus_alloc'] = jobdf.tres_alloc_str.str.extract(r'cpu=(?P<cpus_alloc>[0-9]+)').fillna(0).astype(int) 
 		jobdf['mem_alloc'] = jobdf.tres_alloc_str.str.extract(r'mem=(?P<mem_alloc>[0-9]+)').fillna(0).astype(int) 
-		jobdf['node_alloc'] = jobdf.tres_alloc_str.str.extract(r'node=(?P<node_alloc>[0-9]+)').fillna(0).astype(int) 
+		jobdf['nodes_alloc'] = jobdf.tres_alloc_str.str.extract(r'node=(?P<nodes_alloc>[0-9]+)').fillna(0).astype(int) 
 		# Tidy up the columns
 		jobdf.drop(columns=['user_id', 'tres_req_str', 'tres_alloc_str'], inplace=True)
 		jobdf.rename(columns={'job_state': 'state'}, inplace=True)
@@ -45,19 +45,19 @@ class JobInfoCollector(object):
 		# Update the metrics
 		jobdf.apply(lambda row: [
 				JOBS_NUM.add_metric(row[self.labels], row['job_num']),
-				JOBS_CPU_REQ.add_metric(row[self.labels], row['cpu_req']),
-				JOBS_CPU_ALLOC.add_metric(row[self.labels], row['cpu_alloc']),	
+				JOBS_CPUS_REQ.add_metric(row[self.labels], row['cpus_req']),
+				JOBS_CPUS_ALLOC.add_metric(row[self.labels], row['cpus_alloc']),	
 				JOBS_MEM_REQ.add_metric(row[self.labels], row['mem_req']),
 				JOBS_MEM_ALLOC.add_metric(row[self.labels], row['mem_alloc']),	
-				JOBS_NODE_REQ.add_metric(row[self.labels], row['node_req']),
-				JOBS_NODE_ALLOC.add_metric(row[self.labels], row['node_alloc']),	
+				JOBS_NODES_REQ.add_metric(row[self.labels], row['nodes_req']),
+				JOBS_NODES_ALLOC.add_metric(row[self.labels], row['nodes_alloc']),	
 			], axis=1, raw=True
 		)
 		yield JOBS_NUM
-		yield JOBS_CPU_REQ
-		yield JOBS_CPU_ALLOC
+		yield JOBS_CPUS_REQ
+		yield JOBS_CPUS_ALLOC
 		yield JOBS_MEM_REQ
 		yield JOBS_MEM_ALLOC
-		yield JOBS_NODE_REQ
-		yield JOBS_NODE_ALLOC
+		yield JOBS_NODES_REQ
+		yield JOBS_NODES_ALLOC
 
